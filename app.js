4a745a23130c673634d5920a3ab02b08cb8d4c3c
Thibault CORDIER <cordier@abvent.fr>
2013-11-15 17:43:13 +0100

    CSRF + Form + ORM

--- app.js
+++ app.js
@@ -2,70 +2,149 @@ var port = 3000;
 
 var express = require('express');
 var twig = require('twig');
-
 var app = express();
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize('nodejs', 'root', 'En954Rmbde!!', {
+  host: "localhost",
+  port: 3306,
+  logging: false,
+  dialect: 'mysql'
+})
+
+var Post = sequelize.define('Post', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  title: {
+    type: Sequelize.STRING,
+  },
+  content: {
+    type: Sequelize.TEXT,
+  }
+})
+Post.sync({force: true});
 
 app.set("twig options", {
-    strict_variables: false
+  strict_variables: false
 });
 
 app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'twig');
 app.use(express.logger('dev'));
-app.use(express.cookieParser('tobo!'));
-app.use(express.cookieSession({ secret: 'tobo!'}));
+app.use(express.cookieParser());
+app.use(express.cookieSession({ secret: 'aa'}));
+
+
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(express.session());
+
+
+// app.use(function(req, res, next){
+//   next();
+// });
+
+app.use(function(req, res, next) {
+
+  req.models = {};
+  req.models.Post = Post;
+  next();
+});
+
+app.use(express.csrf());
+
+app.use(function(req, res, next){
+  res.locals.token = req.csrfToken();
+  next();
+});
+
 app.use(app.router);
 app.use(express.compress());
 app.use(express.methodOverride());
-app.use(express.bodyParser());
-
-app.use(express.favicon());
 
 
-app.use(express.static(__dirname + '/public'))
-app.use(express.bodyParser());
+
+// Error Handling
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  if(err.status == "403") {
+    res.send(err.status, "Oops.. Access Denied");
+  } else {
+    res.send(err.status, "Oops... Something is broken");
+  }
+});
+
+
 
 // Twig.js has support for this using the {% extends parent %} tag
 app.set("view options", { layout: false });
 
-
 app.set('title', 'Sogox Project');
 
 
-app.get('/pages/*', function(req, res, next){
-    res.render('homepage/index.twig', { url: req.url });
-    return;
+app.all('/post/new', function(req, res, next){
+  if(typeof req.body.title === 'undefined' || typeof req.body.content === 'undefined') {
+   //res.json(406,{ error: 'One field is missing..' });
+   return res.render('post/new.twig', { url: req.url, csrf: res.locals.token });
+ } else if(req.body.title == '' || req.body.content == '') {
+   //res.json(406,{ error: 'One value is empty..' });
+   return res.render('post/new.twig', { url: req.url, csrf: res.locals.token });
+ } else {
+  req.models.Post.create({ title: req.body.title, content: req.body.content })
+  .success(function(post) {
+    //res.json(201,{ 'project': project });
+    res.redirect(301, '/');
+    return ;
+  })
+  .error(function(err) {
+    console.log(err);
+    //res.json(406,{ error: err.message });
+    return res.render('post/new.twig', { url: req.url, csrf: res.locals.token });
+    //return ;
+  });
+}
 });
-app.get('/api/*', function(req, res, next){
-    return res.json({ txt: 'plop' });
+
+
+
+app.get('/post/:id', function(req, res, next){
+  var id = req.params.id;
+  console.log(id);
+  req.models.Post.find(id)
+  .success(function(post) {
+    if(post === null) {
+      console.log(post);
+      res.redirect(301, '/');
+    } else {
+     return res.render('post/post.twig', { url: req.url, csrf: res.locals.token, 'post': post });
+   }
+ });
 });
 
-app.get('*', function(req, res, next){
-    res.render('homepage/index.twig', { url: req.url });
-    return;
+
+app.get('/', function(req, res, next){
+  req.models.Post.findAll().success(function(posts) {
+    return res.render('homepage/index.twig', { url: req.url, csrf: res.locals.token, 'posts': posts });
+  })
+  .error(function(err) {
+    res.json(406,{ error: err.message });
+  })
+  ;
+
 });
 
 
+app.all('*', function(req, res, next){
+  res.redirect(301, '/');
+  // res.render('homepage/index.twig', { url: req.url, csrf: res.locals.token });
+  return;
+});
 
-// app.use(function(req, res, next){
-//   res.status(404);
-
-//   // respond with html page
-//   if (req.accepts('html')) {
-//     res.render('Errors/404.twig', { url: req.url });
-//     return;
-//   }
-
-//   // respond with json
-//   if (req.accepts('json')) {
-//     res.send({ error: 'Not found' });
-//     return;
-//   }
-
-//   // default to plain-text. send()
-//   res.type('txt').send('Not found');
-// });
 
 
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,150 +1,150 @@
 
-Twig.cache = true;
+Twig.cache = false;
 
 (function(window,undefined){
 
-    var supercount = 0;
-    var base = "/views/"
-    api_base = "/api";
+    // var supercount = 0;
+    // var base = "/views/"
+    // api_base = "/api";
 
-    crossroads.addRoute('/', function(){
-        // Load notes page
-        var template = twig({ref: "index"})
-        , output = template.render({json:true});
+    // crossroads.addRoute('/', function(){
+    //     // Load notes page
+    //     var template = twig({ref: "index"})
+    //     , output = template.render({json:true});
 
-        $("#noteApp").html(output);
-    });
+    //     $("#noteApp").html(output);
+    // });
 
-    crossroads.addRoute('/toto', function(){
-        // Load notes page
-        var template = twig({ref: "toto"});
-        var url = api_base + '/test';
-        $.getJSON(url, function(data) {
-            var output = template.render({json:true,
-              count: supercount,
-              data: data
-          });
+    // crossroads.addRoute('/toto', function(){
+    //     // Load notes page
+    //     var template = twig({ref: "toto"});
+    //     var url = api_base + '/test';
+    //     $.getJSON(url, function(data) {
+    //         var output = template.render({json:true,
+    //           count: supercount,
+    //           data: data
+    //       });
 
-            $("#noteApp").html(output);
-        });
-        supercount++;
+    //         $("#noteApp").html(output);
+    //     });
+    //     supercount++;
 
-    });
+    // });
 
-    crossroads.addRoute('/notes', function(){
-        // Load notes page
-        var template = twig({ref: "notes"})
-        , url = api_base + "/notes";
+    // crossroads.addRoute('/notes', function(){
+    //     // Load notes page
+    //     var template = twig({ref: "notes"})
+    //     , url = api_base + "/notes";
 
-        $.getJSON(url, function(data) {
-            var output = template.render(data);
-            $("#noteApp").html(output);
-        });
-    });
+    //     $.getJSON(url, function(data) {
+    //         var output = template.render(data);
+    //         $("#noteApp").html(output);
+    //     });
+    // });
 
-    crossroads.addRoute('/notes/{id}', function(id) {
-        // Load notes page
-        var template = twig({ref: "note"})
-        , error_template = twig({ref: "404"})
-        , url = api_base + "/notes/" + id;
+    // crossroads.addRoute('/notes/{id}', function(id) {
+    //     // Load notes page
+    //     var template = twig({ref: "note"})
+    //     , error_template = twig({ref: "404"})
+    //     , url = api_base + "/notes/" + id;
 
-        $.getJSON(url, function(data) {
-            var output;
-            if (data.error) {
-                output = error_template.render(data);
-            } else {
-                output = template.render(data);
-            }
-            $("#noteApp").html(output);
-        });
-    });
+    //     $.getJSON(url, function(data) {
+    //         var output;
+    //         if (data.error) {
+    //             output = error_template.render(data);
+    //         } else {
+    //             output = template.render(data);
+    //         }
+    //         $("#noteApp").html(output);
+    //     });
+    // });
 
-    crossroads.addRoute('/add', function(){
-        // Load notes page
-        var template = twig({ref: "form"})
-        , output = template.render({json:true});
+    // crossroads.addRoute('/add', function(){
+    //     // Load notes page
+    //     var template = twig({ref: "form"})
+    //     , output = template.render({json:true});
 
-        $("#noteApp").html(output);
-    });
+    //     $("#noteApp").html(output);
+    // });
 
-    crossroads.addRoute('/edit/{id}', function(id) {
-        // Load notes page
-        var template = twig({ref: "form"})
-        , error_template = twig({ref: "404"})
-        , url = api_base + "/notes/" + id;
+    // crossroads.addRoute('/edit/{id}', function(id) {
+    //     // Load notes page
+    //     var template = twig({ref: "form"})
+    //     , error_template = twig({ref: "404"})
+    //     , url = api_base + "/notes/" + id;
 
-        $.getJSON(url, function(data) {
-            var output;
-            if (data.error) {
-                output = error_template.render(data);
-            } else {
-                output = template.render(data);
-            }
-            $("#noteApp").html(output);
-        });
-    });
+    //     $.getJSON(url, function(data) {
+    //         var output;
+    //         if (data.error) {
+    //             output = error_template.render(data);
+    //         } else {
+    //             output = template.render(data);
+    //         }
+    //         $("#noteApp").html(output);
+    //     });
+    // });
 
-    // Preload templates
-    (function() {
-        var loaded = 0
-        , count = 2
-        , inc_loaded = function() {
-            loaded++;
-            if (loaded == count) {
-                    // Flag as loaded, signal any waiting events
-                    var State = History.getState();
-                    hash = State.hash;
-                        //console.log(hash);
-                        crossroads.parse(hash);
+    // // Preload templates
+    // (function() {
+    //     var loaded = 0
+    //     , count = 2
+    //     , inc_loaded = function() {
+    //         loaded++;
+    //         if (loaded == count) {
+    //                 // Flag as loaded, signal any waiting events
+    //                 var State = History.getState();
+    //                 hash = State.hash;
+    //                     //console.log(hash);
+    //                     crossroads.parse(hash);
 
-                    }
-                }
-                , pages = {
-                // "note": "pages/note.twig"
-                // , "notes": "pages/notes.twig"
-                "index": "pages/index.twig",
-                "toto": "pages/toto.twig"
-                // , "form": "pages/note_form.twig"
-                // , "404": "pages/note_404.twig"
-            };
+    //                 }
+    //             }
+    //             , pages = {
+    //             // "note": "pages/note.twig"
+    //             // , "notes": "pages/notes.twig"
+    //             "index": "pages/index.twig",
+    //             "toto": "pages/toto.twig"
+    //             // , "form": "pages/note_form.twig"
+    //             // , "404": "pages/note_404.twig"
+    //         };
 
-            for (id in pages) {
-                if (pages.hasOwnProperty(id)) {
-                    twig({
-                        id: id
-                        , href: base + pages[id]
-                        , load: function() {
-                            inc_loaded();
-                        }
-                    });
-                };
-            }
+    //         for (id in pages) {
+    //             if (pages.hasOwnProperty(id)) {
+    //                 twig({
+    //                     id: id
+    //                     , href: base + pages[id]
+    //                     , load: function() {
+    //                         inc_loaded();
+    //                     }
+    //                 });
+    //             };
+    //         }
 
-        })();
+    //     })();
 
 
-        var History = window.History;
-    // Don't bind AJAX events without history support
-    if ( !History.enabled ) {
-        return false;
-    }
+    //     var History = window.History;
+    // // Don't bind AJAX events without history support
+    // if ( !History.enabled ) {
+    //     return false;
+    // }
 
-    $(function() {
-        // Bind to StateChange Event
-        History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
-            var State = History.getState()
-            , hash = State.hash;
+    // $(function() {
+    //     // Bind to StateChange Event
+    //     History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+    //         var State = History.getState()
+    //         , hash = State.hash;
 
-            //console.log(hash);
-            // Trigger router
-            crossroads.parse(hash);
-        });
+    //         //console.log(hash);
+    //         // Trigger router
+    //         crossroads.parse(hash);
+    //     });
 
-        // Bind to links
-        $("a.ajax_link").live("click", function(event) {
-            event.preventDefault();
-            var href = $(this).attr("href");
-            History.pushState(null, null, href);
-        });
-    });
+    //     // Bind to links
+    //     $("a.ajax_link").live("click", function(event) {
+    //         event.preventDefault();
+    //         var href = $(this).attr("href");
+    //         History.pushState(null, null, href);
+    //     });
+    // });
 })(window);
